e6c571cb7cce8c1088a65c5efc3575e1ab02b9ff
GonzaLmas <gonzalolamas12@gmail.com>
2023-04-29 15:38:01 -0300

    suma dos números ingresados, message box

---
+++ Ejercicio1/SumaDosNros/frmSuma.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SumaDosNros
+{
+    partial class frmSuma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.lblMas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(54, 28);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(177, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Ingrese un número en cada espacio";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(31, 90);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(156, 90);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 2;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Location = new System.Drawing.Point(103, 172);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(75, 23);
+            this.btnSumar.TabIndex = 3;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // lblMas
+            // 
+            this.lblMas.AutoSize = true;
+            this.lblMas.Location = new System.Drawing.Point(137, 93);
+            this.lblMas.Name = "lblMas";
+            this.lblMas.Size = new System.Drawing.Size(13, 13);
+            this.lblMas.TabIndex = 4;
+            this.lblMas.Text = "+";
+            // 
+            // frmSuma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 249);
+            this.Controls.Add(this.lblMas);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblTitulo);
+            this.MaximumSize = new System.Drawing.Size(302, 288);
+            this.MinimumSize = new System.Drawing.Size(302, 288);
+            this.Name = "frmSuma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sumador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label lblMas;
+    }
+}
+
---
+++ Ejercicio1/SumaDosNros/frmSuma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumaDosNros
+{
+    public partial class frmSuma : Form
+    {
+        public frmSuma()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text.Length == 0 && txt1.Text.Length == 0)
+                MessageBox.Show("Por favor complete los dos campos con números.", "Advertencia");
+            else
+            {
+                int n1 = int.Parse(txt1.Text);
+                int n2 = int.Parse(txt2.Text);
+
+                int res = n1 + n2;
+
+                MessageBox.Show("La suma de los números ingresados es: " + res, "Resultado");
+            }
+        }
+    }
+}
