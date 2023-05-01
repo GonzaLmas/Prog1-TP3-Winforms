3eba7dbdccc1799e4448b12a02eeaff976d09ae6
GonzaLmas <gonzalolamas12@gmail.com>
2023-05-01 18:30:28 -0300

    tipo calculadora

---
+++ Ejercicio4/Calculadora/frmCalc.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Calculadora
+{
+    partial class frmCalc
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnPotencia = new System.Windows.Forms.Button();
+            this.btnRaiz = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(65, 69);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 0;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(241, 69);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 1;
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Location = new System.Drawing.Point(65, 126);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(100, 23);
+            this.btnSuma.TabIndex = 2;
+            this.btnSuma.Text = "Sumar";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Location = new System.Drawing.Point(241, 126);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(100, 23);
+            this.btnResta.TabIndex = 3;
+            this.btnResta.Text = "Restar";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(65, 164);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(100, 23);
+            this.btnMultiplicar.TabIndex = 4;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(241, 164);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(100, 23);
+            this.btnDividir.TabIndex = 5;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnPotencia
+            // 
+            this.btnPotencia.Location = new System.Drawing.Point(65, 202);
+            this.btnPotencia.Name = "btnPotencia";
+            this.btnPotencia.Size = new System.Drawing.Size(100, 37);
+            this.btnPotencia.TabIndex = 6;
+            this.btnPotencia.Text = "Potencia (caja 1)";
+            this.btnPotencia.UseVisualStyleBackColor = true;
+            this.btnPotencia.Click += new System.EventHandler(this.btnPotencia_Click);
+            // 
+            // btnRaiz
+            // 
+            this.btnRaiz.Location = new System.Drawing.Point(241, 202);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(100, 37);
+            this.btnRaiz.TabIndex = 7;
+            this.btnRaiz.Text = "Raiz Cuadrada (caja 1)";
+            this.btnRaiz.UseVisualStyleBackColor = true;
+            this.btnRaiz.Click += new System.EventHandler(this.btnRaiz_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(12, 29);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(370, 13);
+            this.lblTitulo.TabIndex = 8;
+            this.lblTitulo.Text = "Ingrese un número en cada caja. Luego elija la operación que quiera realizar.";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(152, 260);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(58, 13);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Caja 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(95, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Caja 1";
+            // 
+            // frmCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 297);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnRaiz);
+            this.Controls.Add(this.btnPotencia);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "frmCalc";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnPotencia;
+        private System.Windows.Forms.Button btnRaiz;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ejercicio4/Calculadora/frmCalc.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class frmCalc : Form
+    {
+        public frmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txt1.Text);
+            int n2 = int.Parse(txt2.Text);
+
+            int res = n1 + n2;
+
+            lblResultado.Text = "Resultado: " + res.ToString();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txt1.Text);
+            int n2 = int.Parse(txt2.Text);
+
+            int res = n1 - n2;
+
+            lblResultado.Text = "Resultado: " + res.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txt1.Text);
+            int n2 = int.Parse(txt2.Text);
+
+            int res = n1 * n2;
+
+            lblResultado.Text = "Resultado: " + res.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txt1.Text);
+            int n2 = int.Parse(txt2.Text);
+
+            if (n2 != 0)
+            {
+                float res = n1 / n2;
+
+                lblResultado.Text = "Resultado: " + res.ToString();
+            }
+        }
+
+        private void btnPotencia_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text.Length != 0 && txt2.Text.Length == 0)
+            {
+                int n1 = int.Parse(txt1.Text);
+                int res = n1 * n1;
+
+                lblResultado.Text = "Resultado: " + res.ToString();
+            }
+            else
+            {
+                MessageBox.Show("El número debe ingresarlo en la caja 1");
+                txt2.Text = "";
+            }
+        }
+
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            if (txt1.Text.Length != 0 && txt2.Text.Length == 0)
+            {
+                int n1 = int.Parse(txt1.Text);
+                double res = Math.Sqrt(n1);
+
+                lblResultado.Text = "Resultado: " + res.ToString();
+            }
+            else
+            {
+                MessageBox.Show("El número debe ingresarlo en la caja 1");
+                txt2.Text = "";
+            }
+        }
+
+    }
+}
