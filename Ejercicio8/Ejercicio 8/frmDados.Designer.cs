1327c589f0356425cb4c30b43d45e6c3bc3546cf
GonzaLmas <gonzalolamas12@gmail.com>
2023-05-02 23:01:22 -0300

    Simulación tirada de dados

---
+++ Ejercicio8/Ejercicio 8/frmDados.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Ejercicio_8
+{
+    partial class frmDados
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTirar = new System.Windows.Forms.Button();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTirar
+            // 
+            this.btnTirar.Location = new System.Drawing.Point(97, 64);
+            this.btnTirar.Name = "btnTirar";
+            this.btnTirar.Size = new System.Drawing.Size(75, 23);
+            this.btnTirar.TabIndex = 0;
+            this.btnTirar.Text = "Tirar";
+            this.btnTirar.UseVisualStyleBackColor = true;
+            this.btnTirar.Click += new System.EventHandler(this.btnTirar_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Location = new System.Drawing.Point(94, 123);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(58, 13);
+            this.lblRes.TabIndex = 1;
+            this.lblRes.Text = "Resultado:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(31, 24);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(224, 13);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Oprima el botón para simular la tirada del dado";
+            // 
+            // frmDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 178);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.btnTirar);
+            this.Name = "frmDados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simulador dados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTirar;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ Ejercicio8/Ejercicio 8/frmDados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_8
+{
+    public partial class frmDados : Form
+    {
+        public frmDados()
+        {
+            InitializeComponent();
+        }
+        private readonly Random rand = new Random();
+
+        private void btnTirar_Click(object sender, EventArgs e)
+        {
+            int nro = rand.Next(1, 7);
+            lblRes.Text = "Resultado: " + nro.ToString();
+        }
+    }
+}
