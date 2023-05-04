02ae2863ada702ba8e20228642a5e88ab88a3601
GonzaLmas <gonzalolamas12@gmail.com>
2023-05-04 16:38:37 -0300

    seleccionar imagen de directorio y ponerla de fondo del form

---
+++ Ejercicio13/FileDialogPictureBox/frmFile.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace FileDialogPictureBox
+{
+    partial class frmFile
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.lblAbrir = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.AccessibleRole = System.Windows.Forms.AccessibleRole.ButtonDropDownGrid;
+            this.pbxImagen.Location = new System.Drawing.Point(236, 67);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(373, 284);
+            this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImagen.TabIndex = 0;
+            this.pbxImagen.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(73, 81);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 38);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // lblAbrir
+            // 
+            this.lblAbrir.AutoSize = true;
+            this.lblAbrir.Location = new System.Drawing.Point(12, 26);
+            this.lblAbrir.Name = "lblAbrir";
+            this.lblAbrir.Size = new System.Drawing.Size(225, 13);
+            this.lblAbrir.TabIndex = 2;
+            this.lblAbrir.Text = "Oprima el botón \"Abrir\" para elegir una imagen";
+            // 
+            // frmFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 436);
+            this.Controls.Add(this.lblAbrir);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.pbxImagen);
+            this.MaximumSize = new System.Drawing.Size(637, 475);
+            this.MinimumSize = new System.Drawing.Size(637, 475);
+            this.Name = "frmFile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mostrar imagen de un directorio";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxImagen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Label lblAbrir;
+    }
+}
+
---
+++ Ejercicio13/FileDialogPictureBox/frmFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDialogPictureBox
+{
+    public partial class frmFile : Form
+    {
+        public frmFile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Elija una imagen";
+            openFileDialog1.ShowDialog();
+
+
+            pbxImagen.Image = Image.FromFile(openFileDialog1.FileName);
+        }
+    }
+}
