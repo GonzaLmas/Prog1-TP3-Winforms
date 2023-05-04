d1705f8cfff57a76d32e8a1e17860e8b0c6776a2
GonzaLmas <gonzalolamas12@gmail.com>
2023-05-03 21:30:40 -0300

    cambiar form backcolor, colorDialog

---
+++ Ejercicio12/ColorDiagolForm/frmColor.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ColorDiagolForm
+{
+    partial class frmColor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(123, 91);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(4, 22);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(334, 13);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Oprima el botón \"Color\" y elija un color para cambiar el color de fondo";
+            // 
+            // frmColor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 198);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnColor);
+            this.MaximumSize = new System.Drawing.Size(357, 237);
+            this.MinimumSize = new System.Drawing.Size(357, 237);
+            this.Name = "frmColor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cambia color";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ Ejercicio12/ColorDiagolForm/frmColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorDiagolForm
+{
+    public partial class frmColor : Form
+    {
+        public frmColor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+
+            Color col = new Color();
+            col = colorDialog1.Color;
+ 
+            this.BackColor = col;
+        }
+    }
+}
